1231752d32f29f159cd36141b4c42bc0a9f94d67
Michał <michal.wezowski@yahoo.com>
2017-05-07 21:01:49 +0200

    list form component

--- src/app/list-form/list-form.component.ts
+++ src/app/list-form/list-form.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { List } from '../list';
 
 @Component({
   selector: 'app-list-form',
   templateUrl: './list-form.component.html',
-  styleUrls: ['./list-form.component.css']
+  styleUrls: ['./list-form.component.scss']
 })
 export class ListFormComponent implements OnInit {
+  public model  = new List('');
+  public editing = false;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    console.log(this.model);
+  }
 }
