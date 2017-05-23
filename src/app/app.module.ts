999e53c3c4c28a0b8304926a2a7079c97e3ecfc7
Michał <michal.wezowski@yahoo.com>
2017-05-23 22:43:15 +0200

    create list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { ListFormComponent } from './list-form/list-form.component';
 import { Bus } from './bus';
 import { CommandFactory } from './commands/command-factory';
 import { ListService } from './list/list.service';
+import { ListComponent } from './list/list.component';
 
 // @todo not sure if that's right way of making a provider
 @NgModule({
   declarations: [
     AppComponent,
     BoardComponent,
-    ListFormComponent
+    ListFormComponent,
+    ListComponent
   ],
   imports: [
     NgbModule.forRoot(),
