bb3e191279f5be76a9daee158d6cd723b7f61466
Michał <michal.wezowski@yahoo.com>
2017-05-23 23:32:17 +0200

    set global font, edit list name

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -9,10 +9,27 @@ import { List } from '../list';
 export class ListComponent implements OnInit {
 
   @Input()
-  list: List;
-  constructor() { }
+  public list: List;
+  public model   = new List('');
+  public editing = false;
+  public id      = Math.floor(Math.random() * 1000000);
 
   ngOnInit() {
+    this.model.name = this.list.name;
   }
 
+  focus() {
+    this.editing = true;
+  }
+
+  blur() {
+    this.editing = false;
+  }
+
+  onSubmit() {
+    // @todo should i use `List` model here?
+    // this.bus.handle(new CreateListCommand(this.model.name));
+
+    // this.model = new List('');
+  }
 }
